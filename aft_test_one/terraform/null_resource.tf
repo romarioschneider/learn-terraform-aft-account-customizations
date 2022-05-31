a27bcb5cc022f3f422e3af92f9a4fc01efe69d80
romarioschneider <schneidersoft@ukr.net>
2022-05-31 13:19:50 +0300

    add ssm param

--- aft_test_one/terraform/null_resource.tf
+++ aft_test_one/terraform/null_resource.tf
@@ -1,5 +1,11 @@
 resource "null_resource" "test_trigger_1" {
   provisioner "local-exec" {
-    command = "echo null_resource test output #1"
+    command = "echo null_resource test output 1"
   }
+}
+
+resource "aws_ssm_parameter" "test_ssm_param" {
+  name = "/test_param"
+  type = "String"
+  value = "test_value"
 }
