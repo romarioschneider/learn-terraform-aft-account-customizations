24892dd3061922f1ae85b0cc17cd580582c2c762
romarioschneider <schneidersoft@ukr.net>
2022-05-31 12:56:12 +0300

    test customizations trigger #1

--- aft_test_one/terraform/null_resource.tf
+++ aft_test_one/terraform/null_resource.tf
@@ -1,5 +1,5 @@
-resource "null_resource" "test_trigger" {
+resource "null_resource" "test_trigger_1" {
   provisioner "local-exec" {
-    command = "echo null_resource test output"
+    command = "echo null_resource test output #1"
   }
 }
