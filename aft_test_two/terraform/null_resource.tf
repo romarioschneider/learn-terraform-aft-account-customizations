e6d9bc04403ae4a225b48bee8c35ef250335ad35
romarioschneider <schneidersoft@ukr.net>
2022-06-16 01:33:25 +0300

    test TFC: added region

--- aft_test_two/terraform/null_resource.tf
+++ aft_test_two/terraform/null_resource.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
 resource "null_resource" "test_trigger_1" {
   provisioner "local-exec" {
     command = "echo null_resource test output 1"
@@ -5,13 +9,13 @@ resource "null_resource" "test_trigger_1" {
 }
 
 resource "aws_ssm_parameter" "test_ssm_param" {
-  name = "/test_param"
-  type = "String"
+  name  = "/test_param"
+  type  = "String"
   value = "test_value"
 }
 
 resource "aws_ssm_parameter" "test_ssm_param_2" {
-  name = "/test_param_2"
-  type = "String"
+  name  = "/test_param_2"
+  type  = "String"
   value = "test_value_2"
 }
